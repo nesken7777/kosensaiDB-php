00ec6c7794770b55dac6b7db051e389fc9bcbd60
uzak0209 <116805788+uzak0209@users.noreply.github.com>
2023-10-10 19:44:31 +0900

    update変更

--- update.php
+++ update.php
@@ -1,6 +1,6 @@
 
 <?php
-$dsn = 'mysql:dbname=game;host=localhost;port=1234;charset=utf8';
+$dsn = 'mysql:dbname=game;host=localhost;port=3306;charset=utf8';
 $user='uk';
 $password='1234';
 
@@ -27,31 +27,52 @@ class Json {
 
 
 try{
-    $power=$_GET['power'];
-    $score1=$_GET['score1'];
-    $score2=$_GET['score2'];
-    $speed=$_GET['speed'];
-    $stamina=$_GET['stamina'];
-    $luck=$_GET['luck'];
-    $id=$_GET['id'];
+    
+    $power=$_GET['power'] ?? 0;
+    $score1=$_GET['score1'] ?? 0;
+    $score2=$_GET['score2'] ?? 0;
+    $speed=$_GET['speed'] ?? 0;
+    $stamina=$_GET['stamina'] ?? 0;
+    $luck=$_GET['luck'] ?? 0;
+    $id=$_GET['id'] ?? 0;
 
     
     $dbh = new PDO($dsn,$user,$password);
 
 
+    $sql = 
+    $dbh->prepare('select * from status where id = ?');
+    $sql->bindValue(1,$id);
+    $sql->execute();
+    $all = $sql->fetchAll();
+        foreach ($all as $data)
+        {
+
+            $Jsond=new Json(
+                $data['id'],
+                $data['score1'],
+                $data['score2'],
+                $data['speed'],
+                $data['stamina'],
+                $data['luck'],
+                $data['power'],
+                $data['name']
+            );
         
-   
-        // SQL文をセット
+        }
+    
+    
+    // SQL文をセット
     $stmt = $dbh->prepare(' UPDATE status SET power=:power, score1=:score1, score2=:score2,speed=:speed,stamina=:stamina,luck=:luck where id = :id');
-     
+    
         // 値をセット
-    $stmt->bindValue(':id', $id);    
-    $stmt->bindValue(':power', $power);
-    $stmt->bindValue(':score1', $score1);
+    $stmt->bindValue(':id',$id);    
+    $stmt->bindValue(':power', $Jsond->power + $power);
+    $stmt->bindValue(':score1',  $score1);
     $stmt->bindValue(':score2', $score2);
-    $stmt->bindValue(':speed', $speed);
-    $stmt->bindValue(':stamina', $stamina);
-    $stmt->bindValue(':luck', $luck);
+    $stmt->bindValue(':speed', $Jsond->speed + $speed);
+    $stmt->bindValue(':stamina', $Jsond->stamina + $stamina);
+    $stmt->bindValue(':luck', $Jsond->luck + $luck);
 
      
         // SQL実行
